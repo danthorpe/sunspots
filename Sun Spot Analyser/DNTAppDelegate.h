3a2e1aefc9e828e76e04f00d7a621f343f15251a
Daniel Thorpe <dan@blindingskies.com>
2013-02-05 21:03:39 +0000

    Initial Commit

---
+++ Sun Spot Analyser/DNTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DNTAppDelegate.h
+//  Sun Spot Analyser
+//
+//  Created by Daniel Thorpe on 05/02/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sun Spot Analyser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sun Spot Analyser
+//
+//  Created by Daniel Thorpe on 05/02/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DNTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNTAppDelegate class]));
+    }
+}
